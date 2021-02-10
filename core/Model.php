4538c6e01a14dd1dbdae45dce18c4cd29ea56996
illia <meyson.rylit@gmail.com>
2021-02-10 10:20:35 +0200

    Merge branch 'main' of https://github.com/meyson-ii07/illia-mvc-framework
    # Conflicts:
    #	README.md

--- core/Model.php
+++ core/Model.php
@@ -2,6 +2,8 @@
 
 namespace app\core;
 
+use PDO;
+
 abstract class Model
 {
     protected const RULE_REQUIRED = 'required';
@@ -69,5 +71,112 @@ abstract class Model
     public function getErrors($attr) : array
     {
         return key_exists($attr, $this->errors) ? $this->errors[$attr] : [];
+    }
+
+    abstract public static function tableName(): string;
+
+    abstract public function attributes(): array;
+
+    public function save()
+    {
+        $SQL = null;
+        $id = $this->{"id"};
+        $tableName = $this->tableName();
+        $attributes = $this->attributes();
+        $params = array_map(fn($attr) => ":$attr", $attributes);
+
+        if (isset($id)) {
+            $values = array_combine($attributes, $params);
+            $res = [];
+            foreach ($values as $key => $value) {
+                $res[] = $key."=".$value;
+            }
+            $SQL = "UPDATE $tableName 
+            SET ".implode(',', $res)."
+            WHERE id = $id";
+        } else {
+            $SQL = "INSERT INTO $tableName (".implode(',', $attributes).") VALUES(".implode(',', $params).")";
+        }
+
+        $statement = Application::$app->db->prepare($SQL);
+
+        foreach ($attributes as $attribute) {
+            $statement->bindValue(":$attribute", $this->{$attribute});
+        }
+
+        try {
+            $statement->execute();
+        } catch (\Exception  $e) {
+            echo $e->getMessage();
+        }
+
+        return true;
+    }
+
+    public static function delete($id)
+    {
+        $tableName = static::tableName();
+        if (isset($id)) {
+            $SQL = "DELETE FROM $tableName
+            WHERE id = $id";
+            $statement = Application::$app->db->prepare($SQL);
+            try {
+                $statement->execute();
+            } catch (\Exception  $e) {
+                die($e->getMessage());
+            }
+        }
+    }
+
+    public static function findOne(int $id = null)
+    {
+        $tableName = static::tableName();
+        $result = null;
+        if ($id !== null) {
+            $SQL = "SELECT * FROM $tableName
+            WHERE id = $id";
+
+        }
+
+        $statement = Application::$app->db->prepareQuarry($SQL);
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        try {
+            $result = $statement->fetch();
+        } catch (\Exception  $e) {
+            die($e->getMessage());
+        }
+        return $result;
+    }
+    public static function find(array $where = [], int $limit = null, string $order = null)
+    {
+
+        $tableName = static::tableName();
+        $result = null;
+        $SQL = "SELECT * FROM $tableName";
+        if (!empty($where)) {
+            $res = [];
+            foreach ($where as $key => $value) {
+                $res[] = $key."=".$value;
+            }
+
+            $SQL .= " WHERE ".implode(',', $res);
+
+            if ($limit != null) {
+                $SQL .= " LIMIT $limit";
+            }
+
+            if ($order) {
+                $SQL .= " ORDER BY $order";
+            }
+        }
+
+        $statement = Application::$app->db->prepareQuarry($SQL);
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        try {
+            $result = $statement->fetchAll();
+        } catch (\Exception  $e) {
+            die($e->getMessage());
+        }
+        return $result;
     }
 }
--- core/Request.php
+++ core/Request.php
@@ -56,7 +56,7 @@ class Request
         $data = [];
         if ($this->isGet()) {
             foreach ($_GET as $key => $value) {
-                $data[$key] = filter_input(INPUT_GET, $value, FILTER_SANITIZE_SPECIAL_CHARS);
+                $data[$key] = $value;
             }
         }
 
