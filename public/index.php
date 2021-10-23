79ec79d426e46540b0ac21752d11b44d3c6ff160
illia <meyson.rylit@gmail.com>
2021-10-23 17:22:01 +0300

    twig debug mode, minore fix

--- public/index.php
+++ public/index.php
@@ -7,26 +7,44 @@ use Twig\Loader\FilesystemLoader;
 
     $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
     $dotenv->load();
-    $loader = new FilesystemLoader('../views');
-    $twig = new Environment($loader, [
-        'cache' => '../var/cache',
-    ]);
-
     $config = [
-      'db' => [
-        'dsn' => $_ENV['DB_DSN'],
-        'user' => $_ENV['DB_USER'],
-        'password' => $_ENV['DB_PASSWORD'],
-      ],
+        'db' => [
+            'dsn' => $_ENV['DB_DSN'],
+            'user' => $_ENV['DB_USER'],
+            'password' => $_ENV['DB_PASSWORD'],
+        ],
     ];
+    $isDebug = $_ENV['APP_ENV'] == 'debug';
 
-    $app = new Application(dirname(__DIR__), $config);
-    $app->session->setCsrfToken();
-    $app->router->loadRoutes();
-    $app->twig = $twig;
+    if ($isDebug) {
+        $twigConf = [
+                'debug' => true,
+                'cache' => false,
+                'auto_reload' => true,
+            ];
+    } else {
+        $twigConf = [
+            'debug' => false,
+            'cache' => '../var/cache',
+            'auto_reload' => true,
+        ];
+    }
+
     try {
+        $loader = new FilesystemLoader('../views');
+        $twig = new Environment($loader, $twigConf);
+
+
+        $app = new Application(dirname(__DIR__), $config);
+        $app->session->setCsrfToken();
+        $app->router->loadRoutes();
+        $app->twig = $twig;
         $app->run();
     }
     catch (Exception $e) {
-        dd($e->getTraceAsString()."\n".$e->getMessage());
+        if ($isDebug) {
+            dd($e->getTraceAsString()."\n".$e->getMessage());
+        } else {
+            $twig->render('404_.html.twig');
+        }
     }
